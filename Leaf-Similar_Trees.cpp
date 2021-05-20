760dfba01307ba74353e484978d9519908c79f4b
Erna Hulaj <78096644+ernahulaj@users.noreply.github.com>
2021-05-20 11:53:41 +0200

    Update Leaf-Similar_Trees.cpp

--- Leaf-Similar_Trees.cpp
+++ Leaf-Similar_Trees.cpp
@@ -1,3 +1,9 @@
+/*
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -13,15 +19,15 @@ class Solution {
 public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
 
-        vector<int> first;
-        vector<int> second;
+        vector<int> first; // the first tree
+        vector<int> second; // the second tree
         dfs(root1, first);
         dfs(root2, second);
 
-        if (first.size() != second.size())
+        if (first.size() != second.size()) // if the number of elements is different the trees don't have the same leaves
             return false;
 
-        for (int i = 0; i < first.size(); i++) {
+        for (int i = 0; i < first.size(); i++) { // if the leaves are different from one another
             if (first[i] != second[i])
                 return false;
         }
@@ -29,7 +35,7 @@ public:
         return true;
     }
 
-    void dfs(TreeNode* root, vector<int>& lastRow) {
+    void dfs(TreeNode* root, vector<int>& lastRow) { // depth-first search function
 
         if (root == NULL)
             return;
