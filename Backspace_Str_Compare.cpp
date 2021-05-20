81728f202b472ba1390cec5b9bc7ea7924906552
Erna Hulaj <78096644+ernahulaj@users.noreply.github.com>
2021-05-20 11:43:39 +0200

    Update Backspace_Str_Compare.cpp

--- Backspace_Str_Compare.cpp
+++ Backspace_Str_Compare.cpp
@@ -5,10 +5,10 @@ public:
         string rez1, rez2;
         int i = 0;
 
-        while (s[i] != '\0') {
-            if (s[i] >= 'a' && s[i] <= 'z')
+        while (s[i] != '\0') { // traverse the string
+            if (s[i] >= 'a' && s[i] <= 'z') // if character of string is a-z (a letter) we append it to the rez
                 rez1 += s[i];
-            else if (!rez1.empty())
+            else if (!rez1.empty()) // if it is not empty & not a-z it means it is a '#' character, so we delete the previous character
                 rez1.pop_back();
             i++;
         }
@@ -22,8 +22,10 @@ public:
                 rez2.pop_back();
             i++;
         }
-        if (rez1.compare(rez2) == 0)
+        
+        if (rez1.compare(rez2) == 0) // if the strings are the same return true
             return true;
+        
         return false;
     }
 };
